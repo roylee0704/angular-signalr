66a5ea70e47f383377610963ba50483a6b45741f
roylee0704 <roylee0704@gmail.com>
2015-05-19 17:45:03 +0800

    updated signalr-hub, ready to serve as test-mock object

--- test/mock/signalr-hub.js
+++ test/mock/signalr-hub.js
@@ -2,4 +2,39 @@
  * Created by roylee on 18/05/2015.
  */
 
+var $ =  {
+  hubConnection: createMockHubObject
+};
+
+
+function createMockHubObject () {
+  'use strict';
+
+  var hub = {
+    createHubProxy: function (hubName) {
+      return new HubProxy(hubName);
+    }
+  };
+
+  return hub;
+}
+
+
+function HubProxy ( hubName ) {
+  'use strict';
+
+  this._listeners = {};
+
+  this.on = function (ev, fn) {
+    this._listeners[ev] = fn;
+  };
+
+  this.invoke = function (ev, data) {
+    var listener = this._listeners[ev];
+    if(typeof listener === 'function') {
+      listener.apply(null, Array.prototype.slice.call(arguments, 1));
+    }
+  };
+}
+
 
