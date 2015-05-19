d753b7c07f4f12fae8d2c6e00999f43c7f6cb31a
roylee0704 <roylee0704@gmail.com>
2015-05-19 18:46:51 +0800

    updated src file after TDD

--- src/angular-socket-signalr.js
+++ src/angular-socket-signalr.js
@@ -2,14 +2,20 @@ angular.module('roy.socket-signalr', []).
   provider('hubFactory', function() {
     'use strict';
 
-    this.$get = ['$rootScope', function($rootScope) {
+    this.$get = ['$rootScope', '$timeout', function($rootScope, $timeout) {
+      return function hubFactory (socket) {
 
-
-
-      return function hubFactory (options) {
-
+        return {
+          on: function(eventName, callback) {
+            socket.on(eventName, $timeout(function(){
+              callback();
+            }, 0));
+          },
+          invoke: function(ev, data) {
+            socket.invoke.apply(socket, arguments);
+          }
+        };
 
       };
-
     }];
   });
