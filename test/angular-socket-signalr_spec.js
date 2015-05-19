84ee2d0b42f37003615abe04e14bddc57bb22c00
roylee0704 <roylee0704@gmail.com>
2015-05-19 18:47:14 +0800

    updated spec with TDD

--- test/angular-socket-signalr_spec.js
+++ test/angular-socket-signalr_spec.js
@@ -4,11 +4,20 @@ describe('socketFactory', function() {
   beforeEach(module('roy.socket-signalr'));
 
 
-  var hub;
+  var hub,
+      mockedHub, /*server hub*/
+      spy,
+      $timeout;
 
+  beforeEach(inject(function(hubFactory, _$timeout_) {
 
-  beforeEach(inject(function(hubFactory, $rootScope) {
-    hub = hubFactory();
+    mockedHub = window.$.hubConnection().createHubProxy('testHub');
+    spy = jasmine.createSpy('mockedFn');
+
+    hub = hubFactory( mockedHub );
+
+    $timeout = _$timeout_;
+
 
   }));
 
@@ -17,8 +26,41 @@ describe('socketFactory', function() {
 
     it('should apply asynchronously', function () {
 
+      hub.on('event', spy);
+      mockedHub.invoke('event');
+
+      expect(spy).not.toHaveBeenCalled();
+
+      $timeout.flush();
+      expect(spy).toHaveBeenCalled();
+    });
+
+  });
+
+
+  describe('#emit', function() {
+
+    beforeEach(function() {
+
+      spyOn(mockedHub, 'invoke');
 
     });
+
+    it('should call the delegate hub\'s invoke', function() {
+
+      hub.invoke('event', {foo: 'bar'});
+
+      expect(mockedHub.invoke).toHaveBeenCalled();
+    });
+
+    it('should allow multiple data arguments', function() {
+
+      hub.invoke('event', 'x', 'y');
+
+      expect(mockedHub.invoke).toHaveBeenCalledWith('event', 'x', 'y');
+
+    });
+
   });
 
 
