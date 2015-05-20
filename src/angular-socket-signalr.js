6b9e9454872e2f8f035df72bcaf54157714e7e6b
roylee0704 <roylee0704@gmail.com>
2015-05-20 13:37:43 +0800

    updated src with TDD

--- src/angular-socket-signalr.js
+++ src/angular-socket-signalr.js
@@ -3,8 +3,18 @@ angular.module('roy.socket-signalr', []).
     'use strict';
 
     this.$get = ['$rootScope', '$timeout', function($rootScope, $timeout) {
-      return function hubFactory (socket) {
 
+      var asyncAngularify  = function(socket, callback) {
+
+        return (typeof callback === 'function')? function() {
+          var args = arguments;
+          $timeout(function(){
+            callback.apply(socket, args);
+          }, 0);
+        }: angular.noop;
+      };
+
+      return function hubFactory (socket) {
         return {
           connect: function(transObj) {
             socket.connection.start(transObj);
@@ -13,12 +23,13 @@ angular.module('roy.socket-signalr', []).
             socket.connection.stop();
           },
           on: function(eventName, callback) {
-            socket.proxy.on(eventName, $timeout(function(){
-              callback();
-            }, 0));
+            socket.proxy.on(eventName, asyncAngularify(socket, callback));
           },
           invoke: function(ev, data) {
             socket.proxy.invoke.apply(socket, arguments);
+          },
+          error: function(callback) {
+            socket.connection.error(asyncAngularify(socket, callback));
           }
         };
 
